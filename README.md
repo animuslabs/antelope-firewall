e774e40e1555da9893463bf627d5590f34527f9d
Matthew Jurenka <matt.jurenka@comcast.net>
2023-07-13 21:57:20 -0700

    making antelope-firewall a cli and updating README

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
 # antelope-firewall Milestone 1
 
+This repo contains two crates, antelope-firewall and antelope-firewall-lib. antelope-firewall-lib is a framework that allows a developer to more easily write their own ratelimiter,
+and antelope-firewall is a simple cli wrapper for the basic configuration of antelope-firewall-lib.
+
 ## Building
 ### Dependencies
 `sudo apt install openssl libssl-dev`
 ### Build
-`cargo build`
+`cargo build --release --bin antelope-firewall`
 
 ## Running
 
-We are not providing prebuilt binaries for this project for milestone 1.
-In order to run, please run either `cargo run` or `cargo build --release`, then `./target/release/antelope-firewall --config /path/to/config`.
+We are not providing prebuilt binaries for this project for milestone 1 and 2.
+In order to run, please run either `cargo run --bin antelope-firewall -- --config /path/to/config` or `cargo build --release --bin antelope-firewall`, then `./target/release/antelope-firewall --config /path/to/config`.
 Please ensure ensure that the config file is in toml format, following the example config file `test/example.toml`
 
 ## Prometheus
--- antelope-firewall/src/main.rs
+++ antelope-firewall/src/main.rs
@@ -1,7 +1,34 @@
 use antelope_firewall_lib::{firewall_builder::{AntelopeFirewall, RoutingModeState, AntelopeFirewallError}, config::{from_config, Config}};
+use clap::Parser;
+use command_line::Args;
+
+mod command_line;
 
 #[tokio::main]
-async fn main() -> Result<(), AntelopeFirewallError> {
-    let firewall = from_config(toml::from_str::<Config>(&std::fs::read_to_string("./test/example.toml").unwrap()).unwrap()).await.unwrap();
-    firewall.build().run().await
+async fn main() {
+    let args = Args::parse();
+
+    match &std::fs::read_to_string(args.config.clone()) {
+        Ok(contents) => {
+            match toml::from_str::<Config>(contents) {
+                Ok(parsed) => {
+                    match from_config(parsed).await {
+                        Ok(firewall) => {
+                            firewall.build().run().await;
+                        },
+                        Err(e) => {
+                            println!("Error occurred while running firewall.")
+                        },
+                    }
+                },
+                Err(e) => {
+                    println!("Unable to parse config {}. Please run antelope-firewall --help for information on how to set the config path.", args.config)
+                }
+            }
+        },
+        Err(_) => {
+            println!("Unable to read config {}. Please run antelope-firewall --help for information on how to set the config path.", args.config)
+        }
+    }
+    
 }
